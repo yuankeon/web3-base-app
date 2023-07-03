9f516d3f2e7f305fc9d236a0cec032ee32a8a5a7
changyuan <yuankeon@126.com>
2023-07-03 18:06:11 +0800

    feat: 添加样式和web3

---
+++ README.md
@@ -0,0 +1,17 @@
+# 本项目用于快速搭建 web3 项目
+
+## 快速开始
+
+1. 安装依赖
+```bash
+npm install
+# or
+yarn
+```
+
+2. 启动本地开发服务
+```bash
+npm run dev
+# or
+yarn dev
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const a = () => {
-    console.log(123)
-  }
 
   return (
     <>
@@ -20,17 +15,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="background-radial-gradient"></div>
     </>
   )
 }
