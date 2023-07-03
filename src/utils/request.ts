f51854186525a203e9f1ddf12b36e85041a514f6
changyuan <yuankeon@126.com>
2023-07-03 17:26:06 +0800

    chore: 初始化项目

---
+++ src/utils/request.ts
@@ -0,0 +1,31 @@
+/**
+ * axios的二次封装：使用请求拦截器与响应拦截器
+ */
+import { message } from 'antd'
+import axios from 'axios'
+
+//第一步:利用axios对象的create方法,去创建axios实例(其他的配置:基础路径、超时的时间)
+const request = axios.create({
+  baseURL: '', // axios请求接口时候自动携带 /api 基础路径
+  timeout: 5000, //超时的时间的设置
+})
+
+// request 实例添加请求拦截器
+request.interceptors.request.use((config) => {
+  return config
+})
+
+//request 实例添加响应拦截器
+request.interceptors.response.use(
+  (response) => {
+    //成功回调 => 简化数据
+    return response.data
+  },
+  (error) => {
+    //失败回调:处理http网络错误
+    message.error({ content: error.response.data.message })
+    return Promise.reject(error)
+  },
+)
+
+export { request }
