2bed3c67e013ec9dd58a257a6bc31a977cb2d6ca
changyuan <yuankeon@126.com>
2023-07-06 09:37:28 +0800

    feat: 添加配置文件

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,23 +1,3 @@
-export const EnvMap: {
-  [key: string]: { http: string; rpc: string; admin: string }
-} = {
-  dev: {
-    http: 'https://dev-legency.defed.finance',
-    rpc: 'https://rpc.ankr.com/polygon_mumbai',
-    admin: '0xC6E78C221e033A2bdfCcFAB8bc990ca00D1c055e',
-  },
-  test: {
-    http: 'https://test-legency.defed.finance',
-    rpc: 'https://rpc.ankr.com/polygon_mumbai',
-    admin: '0x3c813C7E07Ab917D46Da7DFd8B59393F5BBAD305',
-  },
-  prod: {
-    http: 'https://v2.defed.finance',
-    rpc: 'https://rpc.ankr.com/polygon',
-    admin: '0x3c813C7E07Ab917D46Da7DFd8B59393F5BBAD305',
-  },
-}
-
 export const TITLE = [
   {
     title: 'Token',
@@ -37,5 +17,6 @@ export const TITLE = [
   },
 ]
 
+export * from './env'
 export * from './tokens'
 export * from './sign'
