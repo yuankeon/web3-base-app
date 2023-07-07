fa82946ec5d033b8ffd454da5be29fa0e8c68686
changyuan <yuankeon@126.com>
2023-07-07 09:58:34 +0800

    feat: 添加主题

--- src/components/SvgIcon.tsx
+++ src/components/SvgIcon.tsx
@@ -2,15 +2,17 @@ export function SvgIcon({
   width = '16px',
   height = '16px',
   iconName,
+  color,
 }: {
   width?: string
   height?: string
   iconName: string
+  color?: string
 }) {
   return (
     // <!-- href 执行用哪一个图标，属性值固定为 #icon-图标名字 -->
     <svg style={{ width, height }}>
-      <use href={`#icon-${iconName}`} />
+      <use href={`#icon-${iconName}`} fill={color} />
     </svg>
   )
 }
--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <a href="https://github.com/yuankeon/web3-base-app" target="__blank">
       <div className="title">
-        <SvgIcon iconName="github" width="24px" height="24px" />
+        <SvgIcon iconName="github" width="24px" height="24px" color="#8247e5" />
         <span>Quick start of your web3 app</span>
         <div style={{ flexGrow: 1 }} />
         <SvgIcon iconName="arrow-up-right" />
